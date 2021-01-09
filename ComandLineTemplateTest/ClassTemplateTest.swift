e6c85b4c5ddf0544c6cb34a42034e85c834c0d97
Oleksiy Chebotarov <chebotarevalexei@gmail.com>
2021-01-09 21:29:03 +0000

    -Added unit test from previous project

--- ComandLineTemplateTest/ClassTemplateTest.swift
+++ ComandLineTemplateTest/ClassTemplateTest.swift
@@ -72,4 +72,72 @@ class ClassTemplateTest: XCTestCase {
         let dayFiveOutput = getOutput(days: 5)
         XCTAssertEqual(dayFiveOutput, resultDay5)
     }
+    
+    func testwithTwoItems() {
+        let items = [Item(name: "+5 Dexterity Vest", sellIn: 10, quality: 20),
+                     Item(name: "Aged Brie", sellIn: 2, quality: 0)]
+        let days = 2
+        let gildedRose = GildedRose(items: items)
+
+        var outcome = ""
+        for _ in 0 ..< days {
+            for item in items {
+                outcome += item.description + "\n"
+            }
+            gildedRose.updateQuality()
+        }
+        XCTAssertEqual("+5 Dexterity Vest, 10, 20\nAged Brie, 2, 0\n+5 Dexterity Vest, 9, 19\nAged Brie, 1, 1\n", outcome)
+    }
+
+    func testWithEightItems2Days() {
+        let items = [
+            Item(name: "+5 Dexterity Vest", sellIn: 10, quality: 20),
+            Item(name: "Aged Brie", sellIn: 2, quality: 0),
+            Item(name: "Elixir of the Mongoose", sellIn: 5, quality: 7),
+            Item(name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80),
+            Item(name: "Sulfuras, Hand of Ragnaros", sellIn: -1, quality: 80),
+            Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 15, quality: 20),
+            Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 10, quality: 49),
+            Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 5, quality: 49),
+        ]
+
+        let days = 2
+        let gildedRose = GildedRose(items: items)
+
+        var outcome = ""
+        for _ in 0 ..< days {
+            for item in items {
+                outcome += item.description + "\n"
+            }
+            gildedRose.updateQuality()
+        }
+
+        XCTAssertEqual("+5 Dexterity Vest, 10, 20\nAged Brie, 2, 0\nElixir of the Mongoose, 5, 7\nSulfuras, Hand of Ragnaros, 0, 80\nSulfuras, Hand of Ragnaros, -1, 80\nBackstage passes to a TAFKAL80ETC concert, 15, 20\nBackstage passes to a TAFKAL80ETC concert, 10, 49\nBackstage passes to a TAFKAL80ETC concert, 5, 49\n+5 Dexterity Vest, 9, 19\nAged Brie, 1, 1\nElixir of the Mongoose, 4, 6\nSulfuras, Hand of Ragnaros, 0, 80\nSulfuras, Hand of Ragnaros, -1, 80\nBackstage passes to a TAFKAL80ETC concert, 14, 21\nBackstage passes to a TAFKAL80ETC concert, 9, 50\nBackstage passes to a TAFKAL80ETC concert, 4, 50\n", outcome)
+    }
+
+    func testWithEightItems5Dys() {
+        let items = [
+            Item(name: "+5 Dexterity Vest", sellIn: 10, quality: 20),
+            Item(name: "Aged Brie", sellIn: 2, quality: 0),
+            Item(name: "Elixir of the Mongoose", sellIn: 5, quality: 7),
+            Item(name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80),
+            Item(name: "Sulfuras, Hand of Ragnaros", sellIn: -1, quality: 80),
+            Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 15, quality: 20),
+            Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 10, quality: 49),
+            Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 5, quality: 49),
+        ]
+
+        let days = 5
+        let gildedRose = GildedRose(items: items)
+
+        var outcome = ""
+        for _ in 0 ..< days {
+            for item in items {
+                outcome += item.description + "\n"
+            }
+            gildedRose.updateQuality()
+        }
+
+        XCTAssertEqual("+5 Dexterity Vest, 10, 20\nAged Brie, 2, 0\nElixir of the Mongoose, 5, 7\nSulfuras, Hand of Ragnaros, 0, 80\nSulfuras, Hand of Ragnaros, -1, 80\nBackstage passes to a TAFKAL80ETC concert, 15, 20\nBackstage passes to a TAFKAL80ETC concert, 10, 49\nBackstage passes to a TAFKAL80ETC concert, 5, 49\n+5 Dexterity Vest, 9, 19\nAged Brie, 1, 1\nElixir of the Mongoose, 4, 6\nSulfuras, Hand of Ragnaros, 0, 80\nSulfuras, Hand of Ragnaros, -1, 80\nBackstage passes to a TAFKAL80ETC concert, 14, 21\nBackstage passes to a TAFKAL80ETC concert, 9, 50\nBackstage passes to a TAFKAL80ETC concert, 4, 50\n+5 Dexterity Vest, 8, 18\nAged Brie, 0, 2\nElixir of the Mongoose, 3, 5\nSulfuras, Hand of Ragnaros, 0, 80\nSulfuras, Hand of Ragnaros, -1, 80\nBackstage passes to a TAFKAL80ETC concert, 13, 22\nBackstage passes to a TAFKAL80ETC concert, 8, 50\nBackstage passes to a TAFKAL80ETC concert, 3, 50\n+5 Dexterity Vest, 7, 17\nAged Brie, -1, 4\nElixir of the Mongoose, 2, 4\nSulfuras, Hand of Ragnaros, 0, 80\nSulfuras, Hand of Ragnaros, -1, 80\nBackstage passes to a TAFKAL80ETC concert, 12, 23\nBackstage passes to a TAFKAL80ETC concert, 7, 50\nBackstage passes to a TAFKAL80ETC concert, 2, 50\n+5 Dexterity Vest, 6, 16\nAged Brie, -2, 6\nElixir of the Mongoose, 1, 3\nSulfuras, Hand of Ragnaros, 0, 80\nSulfuras, Hand of Ragnaros, -1, 80\nBackstage passes to a TAFKAL80ETC concert, 11, 24\nBackstage passes to a TAFKAL80ETC concert, 6, 50\nBackstage passes to a TAFKAL80ETC concert, 1, 50\n", outcome)
+    }
 }
